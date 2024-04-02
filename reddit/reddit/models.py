38d9d0bd9f9de29d2c3335a181f47f308db68465
Ne0n2018 <beevertm@gmail.com>
2024-04-02 19:19:05 +0300

    models

--- reddit/reddit/models.py
+++ reddit/reddit/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 class User(models.Model):
+    first_name = models.CharField(verbose_name="Имя", max_length=50)
+    last_name = models.CharField(verbose_name="Фамилия", max_length=50)
+    email = models.EmailField(verbose_name="E-mail")
+    address = models.CharField(verbose_name="Адрес", max_length=250)
